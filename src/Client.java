afbe4475a2312023bb125d45c4bee4b84305e9ff
Aye10032 <yeyu6_505@126.com>
2018-09-03 12:23:34 +0800

    完成client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,16 @@
+import util.Send;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+        InetAddress inet = InetAddress.getByName(/*"106.12.35.79"*/"127.0.0.1");
+        Socket client = new Socket(inet, 6655);
+
+        new Thread(new Send(client)).start();
+    }
+
 }
