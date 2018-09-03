795dab7b3c69453df8cb92771d92d019a3b1718d
Aye10032 <yeyu6_505@126.com>
2018-09-03 12:18:27 +0800

    send方法

---
+++ src/util/Receive.java
@@ -0,0 +1,8 @@
+package util;
+
+public class Receive implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/util/Send.java
@@ -0,0 +1,61 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Send implements Runnable{
+
+    private BufferedReader consol;
+    private DataOutputStream dos;
+    private boolean isRunning = true;
+
+    @Override
+    public void run() {
+        while (isRunning){
+            send();
+        }
+    }
+
+    public Send(){
+        consol = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public Send(Socket client){
+        this();
+        try {
+            dos = new DataOutputStream(client.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            isRunning = true;
+            CloseUtil.closeAll(dos,consol);
+        }
+    }
+
+    private String getText(){
+        try {
+            return consol.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void send(){
+        String msg = getText();
+        if (null != msg&&msg.equals("")){
+            try {
+                dos.writeUTF(msg);
+                dos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+                isRunning = false;
+                CloseUtil.closeAll(dos,consol);
+
+            }
+        }
+    }
+
+}
